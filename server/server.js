763a7b48e72179d8c704d86c4a697a0b9e13fe66
Marcos Vinicius Nulman <mvsouza7@gmail.com>
2026-09-08 15:15:38 -0300

    feat: enable CORS on the API

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/users', async (req, res) => {
